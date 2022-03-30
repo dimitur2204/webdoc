62ec30483c7423e4df47a511fde2a4d997d9149e
dimitar.nizamov <dimitur2204@gmail.com>
2022-03-30 12:48:12 +0200

    Add josip image and animations

--- index.js
+++ index.js
@@ -1,71 +1,110 @@
 (function indexIIFE() {
-	const $logo = document.querySelector(".transition__logo");
-	const $frameBlack = document.querySelector(".page-transition__black");
-	const $frameRed = document.querySelector(".page-transition__red");
-	const $button = document.querySelector("#button");
+	function homePageAnim() {
+		const $background = document.querySelector(".background");
+		const $frontText = document.querySelector(".front-text");
+		const $firstBtn = document.querySelector("#passion");
+		const $secondBtn = document.querySelector("#dropout");
+		const $thirdBtn = document.querySelector("#yay");
+		const $main = document.querySelector(".content");
+		const tltransition = new TimelineMax({ paused: true })
+			.fromTo(
+				$background,
+				1.6,
+				{
+					autoAlpha: 0,
+					xPercent: -100,
+					clipPath: "polygon(0 0, 83% 0, 83% 100%, 0 100%)",
+				},
+				{
+					autoAlpha: 1,
+					ease: Power3.easeOut,
+					xPercent: 0,
+					clipPath: "polygon(0 0, 62% 0, 51% 100%, 0% 100%)",
+				}
+			)
+			// .fromTo(
+			// 	$firstBtn,
+			// 	1,
+			// 	{ xPercent: -100 },
+			// 	{
+			// 		xPercent: 0,
+			// 		ease: Bounce.easeOut,
+			// 	},
+			// 	"-=0.5"
+			// )
+			// .fromTo(
+			// 	$secondBtn,
+			// 	1,
+			// 	{ xPercent: -100 },
+			// 	{
+			// 		xPercent: 0,
+			// 		ease: Bounce.easeOut,
+			// 	},
+			// 	"-=0.5"
+			// )
+			// .fromTo(
+			// 	$thirdBtn,
+			// 	1,
+			// 	{ xPercent: -100 },
+			// 	{
+			// 		xPercent: 0,
+			// 		ease: Bounce.easeOut,
+			// 	},
+			// 	"-=0.5"
+			// )
+			.fromTo(
+				$main,
+				1,
+				{ xPercent: -100, opacity: 0 },
+				{
+					xPercent: 0,
+					opacity: 1,
+					ease: Power3.easeOut,
+				},
+				"-=1"
+			);
 
-	let tltransition = new TimelineMax({ paused: true })
-		.fromTo(
-			$frameRed,
-			2.2,
-			{ scaleX: 0 },
-			{ scaleX: 1, transformOrigin: "left", ease: Power4.easeInOut }
-		)
-		.fromTo(
-			$frameBlack,
-			2.2,
-			{ scaleX: 0 },
-			{ scaleX: 1, transformOrigin: "left", ease: Power4.easeInOut },
-			0.2
-		)
-		.fromTo(
-			$logo,
-			1.6,
-			{ xPercent: -100, autoAlpha: 0 },
-			{ xPercent: 0, autoAlpha: 1, ease: Power4.easeInOut },
-			0.7
-		)
-		.set($frameRed, { scaleX: 0 });
-	// .to($frameBlack, 2.2, {
-	// 	scaleX: 0,
-	// 	transformOrigin: "right",
-	// 	ease: Power4.easeInOut,
-	// })
-	// .to($logo, 0.2, { autoAlpha: 0 }, "-=1.2");
-	const $scaleUpBtn = document.querySelector("#scaleup-btn");
-	const $pageScaleUpFrame = document.querySelector(".page-scaleup-frame");
-	const $pageScaleUp = document.querySelector(".page-scaleup");
-	const scaleUpBtnCoords = getCoords($scaleUpBtn);
-	let scaleUpTransition = new TimelineMax({ paused: true })
-		.fromTo(
-			$pageScaleUpFrame,
-			2.5,
-			{ scale: 0 },
-			{
-				scale: 1,
-				ease: Power2.easeOut,
-				transformOrigin: `${scaleUpBtnCoords.left}px ${scaleUpBtnCoords.top}px`,
-			}
-		)
-		.fromTo(
-			$pageScaleUp,
-			2.5,
-			{ scale: 0 },
-			{
-				scale: 0.9,
-				ease: Back.easeOut.config(1.8),
-				transformOrigin: `${scaleUpBtnCoords.left}px ${scaleUpBtnCoords.top}px`,
-			},
-			"-=2.2"
-		);
-	document.getElementById("reverse").addEventListener("click", () => {
-		tltransition.reverse();
-		scaleUpTransition.reverse();
-	});
-	$button.addEventListener("click", () => {
-		tltransition.play(0);
-	});
-	$scaleUpBtn.addEventListener("click", () => {
-		scaleUpTransition.play(0);
-	});
+		tltransition.play();
+	}
+	function videoPageAnim() {
+		const $videoPage = document.querySelector(".video-page");
+		const $frameBlack = document.querySelector(".page-transition-white");
+		const $frameRed = document.querySelector(".page-transition-green");
+		const $button = document.querySelector("#button");
+		const $closeVideoBtn = document.querySelector("#close-video");
+
+		const tltransition = new TimelineMax({ paused: true })
+			.fromTo(
+				$frameRed,
+				2.2,
+				{ scaleX: 0 },
+				{ scaleX: 1, transformOrigin: "left", ease: Power4.easeInOut }
+			)
+			.fromTo(
+				$frameBlack,
+				2.2,
+				{ scaleX: 0 },
+				{ scaleX: 1, transformOrigin: "left", ease: Power4.easeInOut },
+				0.2
+			)
+			.fromTo(
+				$videoPage,
+				1.6,
+				{ xPercent: -100, autoAlpha: 0 },
+				{ xPercent: 0, autoAlpha: 1, ease: Power4.easeInOut },
+				0.7
+			)
+			.set($frameRed, { scaleX: 0 });
+
+		$button.addEventListener("click", (e) => {
+			e.preventDefault();
+			tltransition.play();
+		});
+		$closeVideoBtn.addEventListener("click", (e) => {
+			e.preventDefault();
+			tltransition.reverse();
+		});
+	}
+	homePageAnim();
+	videoPageAnim();
 })();
